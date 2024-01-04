5d8900c86c09756bc189cc9d2d4e7289b3d91677
tarsil <tiago.arasilva@gmail.com>
2024-01-04 15:30:19 +0000

    Add initial documentation

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,4 +1,54 @@
+import pytest
+
 from backgrounder.decorator import background
+from backgrounder.tasks import Task
+
+# @background
+# async def work(number):
+#     # Do something expensive here.
+#     return number
+
+
+# @pytest.mark.anyio
+# async def test_decorator_async():
+#     result = await work(2)
+#     assert result == 2
+
+
+@pytest.mark.asyncio
+async def test_task():
+    TOTAL = 0
+
+    async def work(number):
+        # Do something expensive here.
+        nonlocal TOTAL
+        TOTAL = 1
+
+    task = Task(work, 2)
+
+    assert TOTAL == 0
+
+    await task()
+
+    assert TOTAL == 1
+
+
+@pytest.mark.asyncio
+async def test_task_blocking():
+    TOTAL = 0
+
+    def work(number):
+        # Do something expensive here.
+        nonlocal TOTAL
+        TOTAL = 1
+
+    task = Task(work, 2)
+
+    assert TOTAL == 0
+
+    await task()
+
+    assert TOTAL == 1
 
 
 def test_decorator():
